862bb7912d67758a9e056728e0df00d6afeac575
shootingstarfruit <reubencardwell@gmail.com>
2025-04-03 15:02:21 +0100

    Added a page
    Oh the missery

---
+++ chris thank page/ChrisJAVASCRIPT.js
@@ -0,0 +1,45 @@
+function buttonClick1()
+{
+    document.getElementById("hole1").src = "BB1msyOc.jfif";
+    console.log("hole1");
+}
+function buttonClick2()
+{
+    document.getElementById("hole2").src = "BB1msyOc.jfif";
+    console.log("hole2");
+}
+function buttonClick3()
+{
+    document.getElementById("hole3").src = "BB1msyOc.jfif";
+    console.log("hole3");
+}
+function buttonClick4()
+{
+    document.getElementById("hole4").src = "BB1msyOc.jfif";
+    console.log("hole4");
+}
+function buttonClick5()
+{
+    document.getElementById("hole5").src = "BB1msyOc.jfif";
+    console.log("hole5");
+}
+function buttonClick6()
+{
+    document.getElementById("hole6").src = "BB1msyOc.jfif";
+    console.log("hole6");
+}
+function buttonClick7()
+{
+    document.getElementById("hole7").src = "BB1msyOc.jfif";
+    console.log("hole7");
+}
+function buttonClick8()
+{
+    document.getElementById("hole8").src = "BB1msyOc.jfif";
+    console.log("hole8");
+}
+function buttonClick9()
+{
+    document.getElementById("hole9").src = "BB1msyOc.jfif";
+    console.log("hole9");
+}
